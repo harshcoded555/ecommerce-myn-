27ab4bfbb3b9f349e98818bbbfceb9ea5a9f99ce
harsh soni <harshs4400@gmail.com>
2026-04-17 10:10:19 +0530

    corrected images path

--- ecommerceM_react/components/ShowEmpty.jsx
+++ ecommerceM_react/components/ShowEmpty.jsx
@@ -1,7 +1,7 @@
 const ShowEmpty = () => {
     return (<>
         <div className="showempthy" style={{width : "100%",height:"100vh",display : "flex",justifyContent : "center",alignItems:"center"}}>
-            <img src="/public/images/your-cart-is-empty-10330087-8348416.webp" alt="your cart is empty" />    
+            <img src="/images/your-cart-is-empty-10330087-8348416.webp" alt="your cart is empty" />    
         </div>  
     </>)
 };
