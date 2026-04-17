025b34da50edd1c2a80aed3a64be79d505b264ec
harsh soni <harshs4400@gmail.com>
2026-04-17 10:27:58 +0530

    corrected image size for empty cart

--- ecommerceM-react-2/components/ShowEmpty.jsx
+++ ecommerceM-react-2/components/ShowEmpty.jsx
@@ -1,6 +1,6 @@
 const ShowEmpty = () => {
     return (<>
-        <div className="showempthy" style={{width : "100%",height:"100vh",display : "flex",justifyContent : "center",alignItems:"center"}}>
+        <div className="showempthy" style={{width : "100%",display : "flex",justifyContent : "center",alignItems:"center"}}>
             <img src="/images/your-cart-is-empty-10330087-8348416.webp" alt="your cart is empty" />    
         </div>  
     </>)
