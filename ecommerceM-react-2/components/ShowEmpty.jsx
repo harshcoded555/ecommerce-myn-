a97dff6e0c02d3d33dfa6f7861f33c965a1f2dc2
harsh soni <harshs4400@gmail.com>
2026-04-20 20:24:56 +0530

    changed img in ShowEmpty

--- ecommerceM-react-2/components/ShowEmpty.jsx
+++ ecommerceM-react-2/components/ShowEmpty.jsx
@@ -1,6 +1,6 @@
 const ShowEmpty = () => {
     return (<> <div className="showempty" >
-            <img src="/images/your-cart-is-empty-10330087-8348416.webp" alt="your cart is empty" />    
+            <img src="/images/WhatsApp Image 2026-04-20 at 20.19.56.jpeg" alt="your cart is empty" />    
         </div> 
         
     </>)
